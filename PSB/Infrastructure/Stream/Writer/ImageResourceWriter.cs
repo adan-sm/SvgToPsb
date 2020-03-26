4d379f8a93e762a815fdce57e0c83c82975dc079
_adan <sacha.millet@gmail.com>
2020-03-26 21:54:12 +0100

    added ImageResourcesWriter class, updated SectionWriterFactory

---
+++ PSB/Infrastructure/Stream/Writer/ImageResourceWriter.cs
@@ -0,0 +1,8 @@
+﻿namespace Psb.Infrastructure.Stream.Writer
+{
+    internal abstract class ImageResourceWriter
+    {
+        
+        protected abstract void WriteInternal();
+    }
+}
---
+++ PSB/Infrastructure/Stream/Writer/SectionWriters/Implementations/ImageResourcesWriter.cs
@@ -0,0 +1,27 @@
+﻿using Psb.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Psb.Infrastructure.Stream.Writer.SectionWriters.Implementations
+{
+    /// <summary>
+    /// TODO : remove "public"
+    /// </summary>
+    public class ImageResourcesWriter : IImageResourcesWriter
+    {
+        private readonly IBinaryWriter _binaryWriter;
+        private readonly Psb.Domain.IImageResourceList _imageResources;
+
+        public ImageResourcesWriter(IBinaryWriter binaryWriter, IImageResourceList imageResources)
+        {
+            _binaryWriter = binaryWriter ?? throw new ArgumentNullException(nameof(binaryWriter));
+            _imageResources = imageResources ?? throw new ArgumentNullException(nameof(imageResources));
+        }
+
+        public void Write()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
