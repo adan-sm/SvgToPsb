d352601b9fc6459f8068411182a0ea56c783f4db
_adan <sacha.millet@gmail.com>
2020-03-26 11:19:31 +0100

    added ColorModeData class and modify builder according to that

---
+++ PSB/Domain/Implementations/ColorModeData.cs
@@ -0,0 +1,14 @@
+﻿namespace Psb.Domain.Implementations
+{
+    /// <summary>
+    /// TODO : remove "public"
+    /// </summary>
+    public class ColorModeData : IColorModeData
+    {
+        public IPsdFile Owner
+        {
+            get;
+            internal set;
+        }
+    }
+}
